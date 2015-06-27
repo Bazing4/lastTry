9d7ef5021044be6ba985728aaee3c7ea3184a51d
Marco Antonio <marco2yw@hotmail.com>
2015-06-27 16:00:15 -0300

    callback urls do openshift

--- config/auth.js
+++ config/auth.js
@@ -6,19 +6,19 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '922608617799776', // your App ID
         'clientSecret'  : '6aaf2ad06b8fd42a8adaa31ba9ac1fe5', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+        'callbackURL'   : 'http://lasttry-lasttry.rhcloud.com//auth/facebook/callback'
     },
 
     'twitterAuth' : {
         'consumerKey'       : 'BBRtpvAtQs27VaNU03e6YYxTD',
         'consumerSecret'    : 'jekQIU2hy4u8MeR5UsXKFRDz4WGdMTECqZrrqmzqXZovBbETZL',
-        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+        'callbackURL'       : 'http://lasttry-lasttry.rhcloud.com/auth/twitter/callback'
     },
 
     'googleAuth' : {
         'clientID'      : '890401297810-db616jo1jvi3ss02ug7mudjv59gjsfej.apps.googleusercontent.com',
         'clientSecret'  : 'x2PsN21NR1qdTovz0EsQ4DC6',
-        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+        'callbackURL'   : 'http://lasttry-lasttry.rhcloud.com/auth/google/callback'
     }
 
 };
