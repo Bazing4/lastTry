eef23b6731c4a1efcc7b0a5fb027cba3a3f56501
Marco Antonio <marco2yw@hotmail.com>
2015-06-30 10:19:07 -0300

    calculos de teste

--- app/pi.js
+++ app/pi.js
@@ -1,23 +1,50 @@
 module.exports = function() { 
 
-    var PI = {
-
+var PI = {
         somaDoisNumeros : function( num1, num2 ){
 			return Number(num1) + Number(num2);
-		}
-    };
+		},
 
-    return PI;
+        diminuir10: function(numero) {
+                    var valor = Number(numero);
+                    valor -= 1;
+                    valor -= 1;
+                    valor -= 1;
+                    valor -= 1;
+                    valor -= 1;
+                    valor -= 1;
+                    valor -= 1;
+                    valor -= 1;
+                    valor -= 1;
+
+                    return valor;
+        },      
+        
+                diminuir9: function(numero) {
+                    var valor = Number(numero);
+                    valor -= 1;
+                    valor -= 1;
+                    valor -= 1;
+                    valor -= 1;
+                    valor -= 1;
+                    valor -= 1;
+                    valor -= 1;
+                    valor -= 1;
+
+                    return valor;
+        },  
+                diminuir8: function(numero) {
+                    var valor = Number(numero);
+                    valor -= 1;
+                    valor -= 1;
+                    valor -= 1;
+                    valor -= 1;
+                    valor -= 1;
+                    valor -= 1;
+                    valor -= 1;
+
+                    return valor;
+        },  
+            };
+return PI;
 };
-
-module.exports = function() { 
-
-    var soma3 = {
-
-        somaTresNumeros : function( num1, num2, num3 ){
-			return Number(num1) + Number(num2) + Number(num3);
-		}
-    };
-
-    return soma3;
-}; 
--- test/pi.js
+++ test/pi.js
@@ -1,28 +1,37 @@
 var PI = require( "../app/pi.js" )();
-var soma3 = require("../app/pi.js")();
 var expect = require( 'chai' ).expect;
 
 describe( "PI", function() {
 
     
-    describe( "somaDoisNumeros", function() {
+    describe( "Calculos", function() {
         it( "Deveria retornar 5 ao somar 2 + 3", function(){
             var valorEsperado = 5;    
             var valorRetornado = PI.somaDoisNumeros( 2, 3 );
             expect( valorEsperado ).to.equal( valorRetornado );
         });
-    }); 
+        
+        it('Deve retornar 1 quando for passado o numero 10', function() {
+			var numero = 10;
+
+			var resultado = PI.diminuir10(numero);
+            var valorEsperado=1;
+            expect( valorEsperado ).to.equal( resultado );
+		});
+                it('Deve retornar 1 quando for passado o numero 9', function() {
+			var numero = 9;
+
+			var resultado = PI.diminuir9(numero);
+            var valorEsperado=1;
+            expect( valorEsperado ).to.equal( resultado );
+		});
+                it('Deve retornar 1 quando for passado o numero 8', function() {
+			var numero = 8;
+
+			var resultado = PI.diminuir8(numero);
+            var valorEsperado=1;
+            expect( valorEsperado ).to.equal( resultado );
+		});
 });
 
-describe( "PI", function() {
-
-    
-    describe( "somaTresNumeros", function() {
-        it( "Deveria retornar 6 ao somar 2 + 2 + 2", function(){
-            var valorEsperado = 6;    
-            var valorRetornado = soma3.somaTresNumeros( 2, 2, 2 );
-            expect( valorEsperado ).to.equal( valorRetornado );
-        });
-    }); 
 });
-
